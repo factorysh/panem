2aaa03190b3cdeee7fc4a2f81650b57d824890fc
Gael Pasgrimaud <gael@gawel.org>
2018-01-18 11:27:11 +0100

    Back to development: 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panem',
-    version='0.1.0',
+    version='0.1.1.dev0',
     description='panemd',
     url='https://gitlab.bearstech.com/factory/panem',
     install_requires=[
